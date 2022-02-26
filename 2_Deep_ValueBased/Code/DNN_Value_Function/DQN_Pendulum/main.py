1923cba238b8a9e006bb6c2ba502d952632dc375
AngeloGao <wenhan.gao98@gmail.com>
2022-02-26 14:41:17 +0800

    some clean up, dueling not good on Pendulum

--- 2_Deep_ValueBased/Code/DNN_Value_Function/DQN_Pendulum/main.py
+++ 2_Deep_ValueBased/Code/DNN_Value_Function/DQN_Pendulum/main.py
@@ -17,8 +17,8 @@ gamma = 0.95
 sync_freq = 5
 exp_replay_size = 256
 # agent = DQN_agent(seed, layer_sizes, lr, gamma, sync_freq, exp_replay_size)
-# agent = double_DQN_agent(seed, layer_sizes, lr, gamma, sync_freq, exp_replay_size)
-agent = dueling_DQN_agent(seed, layer_sizes, lr, gamma, sync_freq, exp_replay_size)
+agent = double_DQN_agent(seed, layer_sizes, lr, gamma, sync_freq, exp_replay_size)
+# agent = dueling_DQN_agent(seed, layer_sizes, lr, gamma, sync_freq, exp_replay_size)
 
 episodes = 8000
 epsilon = 1.0
